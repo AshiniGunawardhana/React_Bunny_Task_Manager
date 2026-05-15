5762071732d72f6acc105fd2701035088822ac1d
AshiniGunawardhana <141038881+AshiniGunawardhana@users.noreply.github.com>
2026-05-15 18:22:14 +0530

    Header Update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,65 +4,81 @@ import IconButton from "@mui/material/IconButton";
 import DeleteSweepIcon from "@mui/icons-material/DeleteSweep";
 import Tooltip from "@mui/material/Tooltip";
 
-function Header({deleteCompleted}) {
+function Header({ deleteCompleted }) {
   const today = new Date().toLocaleDateString("en-US", {
     month: "long",
     day: "numeric",
     year: "numeric",
   });
 
-    return ( 
+  return (
     <Box
       sx={{
         display: "flex",
         justifyContent: "space-between",
+        flexWrap: "wrap",
         alignItems: "flex-start",
       }}
     >
-    <Box>
-    <Typography 
-           variant="h4" 
-           align="left" 
-           sx={{ fontWeight: 700, color: "text.primary" }} 
-           >
-            Today's Tasks
-          </Typography>
+      <Box>
+        <Typography
+          variant="h4"
+          align="left"
+          sx={{ fontWeight: 700, color: "text.primary" }}
+        >
+          Today's Tasks
+        </Typography>
 
-    <Typography
-        variant="subtitle1"
-        align="left"
+        <Typography
+          variant="subtitle1"
+          align="left"
+          sx={{
+            mt: 0.5,
+            ml: 1,
+            color: "text.secondary",
+            fontWeight: 500,
+          }}
+        >
+          {today}
+        </Typography>
+      </Box>
+
+      <Box
         sx={{
-          mt: 0.5,
-          ml: 1,
-          color: "text.secondary",
-          fontWeight: 500,
-        }}
-      >
-        {today}
-      </Typography>
-    </Box>
+          display: "flex",
+          flexDirection: "column",
+          ml: "auto",
 
-    <Tooltip title="Clear all finished tasks 🧹" arrow>
-    <IconButton
-        onClick={deleteCompleted}
-        sx={{
-          right: "18px",
-          top: "-2px",
-          backgroundColor: "white",
-          border: "2px solid #E58F7C",
-          color: "#E58F7C",
-
-          "&:hover": {
-            backgroundColor: "#E58F7C",
-            color: "white",
+          alignItems: {
+            xs: "center", // 👈 mobile center
+            sm: "center", // keep same or change if needed
           },
+
+          textAlign: "center",
         }}
       >
-        <DeleteSweepIcon sx={{ fontSize: 35 }} />
-      </IconButton>
-      </Tooltip>
+        <Tooltip title="Clear all finished tasks 🧹" arrow>
+          <IconButton
+            onClick={deleteCompleted}
+            sx={{
+              right: "18px",
+              top: "-2px",
+              backgroundColor: "white",
+              border: "2px solid #E58F7C",
+              color: "#E58F7C",
+
+              "&:hover": {
+                backgroundColor: "#E58F7C",
+                color: "white",
+              },
+            }}
+          >
+            <DeleteSweepIcon sx={{ fontSize: 35 }} />
+          </IconButton>
+        </Tooltip>
+      </Box>
     </Box>
-          )
-    }
+  );
+}
 
 export default Header;
