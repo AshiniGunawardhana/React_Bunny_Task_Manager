58ca27ccf676fdb68db65644491b240d50c6dee2
AshiniGunawardhana <141038881+AshiniGunawardhana@users.noreply.github.com>
2026-05-15 17:23:22 +0530

    Update footer
    Add responsiveness to footer text

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,25 +2,37 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 function Footer() {
-    const year = new Date().getFullYear();
-    return ( <Box
+  const year = new Date().getFullYear();
+  return (
+    <Box
       component="footer"
       sx={{
         position: "fixed",
         bottom: 0,
         left: 0,
-        textAlign: "center",
         width: "100%",
+
+        display: "flex",
+        alignItems: "center",
+
+        justifyContent: {
+          xs: "flex-start", // mobile → left side
+          sm: "center", // desktop → center
+        },
+
+        pl: {
+          xs: 2,
+          sm: 0,
+        },
         py: 2,
-        backgroundColor: "#E58F7C", 
+        backgroundColor: "#E58F7C",
         color: "white",
         zIndex: 500,
       }}
     >
-      <Typography variant="body2">
-        Copyright ⓒ {year}
-      </Typography>
-    </Box>);
+      <Typography variant="body2">Copyright ⓒ {year}</Typography>
+    </Box>
+  );
 }
 
 export default Footer;
